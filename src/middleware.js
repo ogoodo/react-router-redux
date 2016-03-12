5a6aa6487419e3f18de673879bc5fe3b07e9e310
ogoodo <ogoodo.com@qq.com>
2016-03-12 13:09:00 +0800

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -5,6 +5,9 @@ import { CALL_HISTORY_METHOD } from './actions'
  * provided history object. This will prevent these actions from reaching your
  * reducer or any middleware that comes after this one.
  */
+ /**
+  * 针对CALL_HISTORY_METHOD, 会调用history的方法 by:ogoodo
+  */
 export default function routerMiddleware(history) {
   return () => next => action => {
     if (action.type !== CALL_HISTORY_METHOD) {
